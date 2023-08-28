16e11b5f4b3b5676d3799c38fc81a72a1aee139a
Elias Xu <124101026+Elias2660@users.noreply.github.com>
2023-08-28 13:54:05 +0800

    additional files

--- CSES/intro_problems/digit_queries.cpp
+++ CSES/intro_problems/digit_queries.cpp
@@ -17,26 +17,25 @@ using std::string;
 int Q; 
 
 int main() {
-    freopen("in", "r", stdin);
+    // freopen("in", "r", stdin);
     cin >> Q;
     for (int i = 0; i < Q; i++) {
-        double K;
-        cin >> K;
-        K--;
-        double digs = 0, space_taken = 0;
-        //find the decimal place that the locs go above
-        if (K > 9) {
-            while (K > (digs + 1) * 9 * pow(10, digs) + space_taken) {
-                digs ++;
-                space_taken += digs * 9 * pow(10, digs - 1);
-            } 
-        }       
-        //for the locs left, find the specific number that it relates to
-        double lastTakenNum = pow(10, digs) - 1;
-        double numbersAbove = floor((K - space_taken)/(digs + 1)); 
-        string num = to_string(lastTakenNum + numbersAbove + 1);
-        
-        double index = fmod((K - space_taken), (digs + 1));
-        cout <<  index << ":" << num  << " : " << fmod(K - space_taken, 10)  << " : " << K  << " : " << space_taken<< endl;
+        long long k,  length = 1;
+        cin >> k;
+        while (k > (length) * 9 * (long long) pow(10, length - 1) ) {
+            long long nextGroup = 9 * (long long) pow(10,(length-1)) * length;
+            k -= nextGroup;
+            length++;
+        }
+        k--;
+        long long remaining = k % length;
+        long long numbersAbove = (k - remaining)/length;
+
+        // cout << spacesLeft << " : " << remaining << " : " << numbersAbove << ": " << length << endl;
+        long long ans_num = (long long)pow(10,length - 1) + numbersAbove;
+        string ans_letter = to_string(ans_num);
+
+        cout << ans_letter[remaining] << endl ;
+        // cout << ans_num << endl;
     }
 }
