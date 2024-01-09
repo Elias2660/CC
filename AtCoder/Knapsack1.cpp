9f5523b84936dc3a45067fc26b89cb3e003cd125
Elias Xu <flam0799@gmail.com>
2024-01-09 14:23:42 -0500

    finish quesiton

--- AtCoder/Knapsack1.cpp
+++ AtCoder/Knapsack1.cpp
@@ -1,8 +1,6 @@
 // https://atcoder.jp/contests/dp/tasks/dp_d
 /*
 ! clang++ -std=c++11 -stdlib=libc++ Knapsack1.cpp
-
-TODO! FINISH PROBLEM !!!
 */
 
 #include <algorithm>
@@ -20,7 +18,6 @@ using std::string;
 using std::vector;
 const int INF = -1e9;
 
-// TODO FINISH PROBLEM !!!
 /*
 This problem is a dp solution
 That means that we're going to have an array with length of the weight; which is
@@ -30,21 +27,23 @@ we're going t
 */
 
 int main() {
+  freopen("input", "r", stdin);
   int N, W;
   cin >> N >> W;
-  vector<int> dp = vector<int>(W, INF);
+  vector<long long> dp = vector<long long>(W+1, INF);
   dp[0] = 0;
   for (int i = 0; i < N; i++) {
     int w, v;
     cin >> w >> v;
     // take in the item values
-    for (int j = W - i; j > 0; j--) {
+    for (int j = W - w + 1; j >= 0; j--) {
       // iterating backwards because you don't want double counting
+      // in addition, we're going to have to check backwards from the earlier things b/c  things
+      // that weight too much don't work
       if (dp[j] != INF) {
         // if the value exists
-
         /*
-        ! to Explain this code :)
+        ! to Explain this code
 
         If the value exists, then we can says that the maximum value of an
         element with the next element with the weight of the current element +
